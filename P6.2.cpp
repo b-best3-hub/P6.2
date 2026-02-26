2ef08dac619f87f44c8459ccf58bc9c34ada70af
benmb <benmb@BENSCOMPUTER>
2026-02-25 21:59:25 -0500

    Add project files.

---
+++ P6.2.cpp
@@ -0,0 +1,65 @@
+/*
+File: P6.2
+Author: Ben Best
+Date: 2026-02-26
+
+Description: Diamond for number of asterisks per side
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int i = 0;       // Diamond row counter
+    int s = 0;       // Diamond space counter
+    int star = 0;    // Diamond star counter
+
+    cout << "Enter number of asterisks per side: ";
+    cin >> n;
+
+    if (n < 2)
+    {
+        cout << "n must be at least 2." << endl;
+        return 0;
+    }
+
+    // Top half and middle of diamond
+    for (i = 1; i <= n; i++)
+    {
+        //Spaces
+        for (s = 1; s <= n - i; s++)
+        {
+            cout << " ";
+        }
+
+        //Stars
+        for (star = 1; star <= (2 * i - 1); star++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Bottom half of diamond
+    for (i = n - 1; i >= 1; i--)
+    {
+        //Spaces
+        for (s = 1; s <= n - i; s++)
+        {
+            cout << " ";
+        }
+
+        //Stars
+        for (star = 1; star <= (2 * i - 1); star++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
